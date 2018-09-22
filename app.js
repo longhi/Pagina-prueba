9b79e07f85906997ccf3d69774db11d55d8c8b95
longhi <longhiv27@gmail.com>
2018-09-22 16:27:14 -0500

    subiendo mejoras

--- app.js
+++ app.js
@@ -1,2 +1,2 @@
-var contenido = prompt("¿Cuales es tu nombre y apellido?", "escirbe aqui respuesta");
+var contenido = prompt("¿Cuales es tu nombre y apellido?", "escirbe aqui respuesta")
 document.getElementById('contenido').innerHTML= "Tu nombre es " + contenido;
